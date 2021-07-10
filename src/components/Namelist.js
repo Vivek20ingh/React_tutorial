2384a60d4f6f173355941f4b521722541d9fa20f
Vivek Singh <vs2357766@gmail.com>
2021-07-11 01:46:03 +0530

    index and key

--- src/components/Namelist.js
+++ src/components/Namelist.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Person from './Person';
-const name =['Vivek','Ankit','Aditya','Rajeev']
-const namelist =   name.map((name) => <h2>{name}</h2>)
+const name =['Vivek','Ankit','Aditya','Rajeev','Vivek']
+const namelist =   name.map((name,index) => <h2 key={index}>{index} {name}</h2>)
 const persons = [
     {id: 1,name: 'Ankit',age :22,skill: 'Android'},
     {id: 2,name: 'Vivek',age :21,skill: 'Web DEvlopment'},
@@ -9,11 +9,11 @@ const persons = [
     {id: 4,name: 'Anuj',age :20,skill: 'Machine Learning'}
     
 ]
-const personslist =persons.map(person => <Person person={person}/>)
+const personslist =persons.map(person => <Person key={person.name} person={person}/>)
 function Namelist() {
     return (
         <div>
-            {name.map((name) => <h2>{name}</h2>)}
+            {/* {name.map((name) => <h2>{name}</h2>)} */}
             {namelist}
             {personslist}
         </div>
--- src/components/Person.js
+++ src/components/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Person({person}) {
     return (
         <div>
-            <h2> I am {person.name}.I am {person.age} year old. I know {person.skill}</h2>
+            <h2> {person.id}: I am {person.name}.I am {person.age} year old. I know {person.skill}</h2>
         </div>
     );
 }
