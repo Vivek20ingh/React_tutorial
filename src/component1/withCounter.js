eb80cc0cd9134bc98cf08ab651d857deff0e5b4b
vivek20ingh <vs2357766@gmail.com>
2021-08-02 00:00:54 +0530

    higer order component

--- src/component1/withCounter.js
+++ src/component1/withCounter.js
@@ -2,9 +2,24 @@ import React from 'react'
 
 const UpdatedComponent=(OriginalComponent)=>{
     class NewComponent  extends React.Component{
+
+        constructor(props) {
+            super(props);
+            this.state={
+                count: 0
+            }
+        }
+    
+        incrementCount =() => {
+            this.setState( prevState => {
+                return {count: prevState.count+1}
+            })
+        }
+        
+
         render()
         {
-            return <OriginalComponent  name='Vivek'/>
+            return <OriginalComponent  name='Vivek' count={this.state.count} incrementCount={this.incrementCount}/>
         }
 
     }
